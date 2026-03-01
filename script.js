d64bb1d0fe4303e862d1d2a1252efdf99868b0ab
Lenny Gitonga <lennygitonga07@gmail.com>
2026-03-01 16:58:47 +0300

    add event listener

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const foodList = document.getElementById('foodList')
 const totalCalories = document.getElementById('totalCalories')
 const resetButton = document.getElementById('resetButton')
 
+form.addEventListener('AddFood', function(event){
+    event.preventDefault()
+    let food = foodInput.value
+    let calories = caloriesInput.value
+
+    foodList.innerHTML +=
+    `
+    <tr>
+        <td>${food}</td>
+        <td>${calories}</td>
+        <td><button>Remove</button></td>
+        </tr>
+        `
+    }
+)
