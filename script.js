b4e81a4558171d3b5069953055bf97a644572c77
Lenny Gitonga <lennygitonga07@gmail.com>
2026-03-01 16:39:51 +0300

    add input form

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form')
+const foodInput = document.getElementById('food')
+const caloriesInput = document.getElementById('calories')
+const foodList = document.getElementById('foodList')
+const totalCalories = document.getElementById('totalCalories')
+const resetButton = document.getElementById('resetButton')
+
